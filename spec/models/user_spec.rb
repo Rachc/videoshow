8bb5548563c6cc89ea6cf234110c44e29867f112
Rachel Curioso <rachelcurioso@gmail.com>
2019-03-30 21:27:32 -0300

    added forgoten user validations and wrote user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'User' do
+    it 'Should have name' do
+      user = User.new(email: 'sailor@moon.com', password: 'SuChH4Ck3R')
+
+      expect(user).to_not be_valid
+    end
+
+    it 'Should have a name with more than 1 character' do
+      user1 = User.new(
+        email: 'sailor@moon.com',
+        password: 'SuChH4Ck3R',
+        name: 'p'
+      )
+
+      user2 = User.new(
+        email: 'sailor@venus.com',
+        password: 'SuChH4Ck3R',
+        name: 'jo'
+      )
+
+      expect(user1).to_not be_valid
+      expect(user2).to be_valid
+    end
+
+    it 'Should have a name with less than 21 chacaters' do
+      user1 = User.new(
+        email: 'sailor@moon.com',
+        password: 'SuChH4Ck3R',
+        name: 'a' * 21
+      )
+      user2 = User.new(
+        email: 'sailor@moon.com',
+        password: 'SuChH4Ck3R',
+        name: 'p' * 20
+      )
+
+      expect(user1).to_not be_valid
+      expect(user2).to be_valid
+    end
+  end
 end
