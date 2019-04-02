e0864421d7fc7afdab241918a1e9b3eef76a0163
Rachel Curioso <rachelcurioso@gmail.com>
2019-04-01 23:32:16 -0300

    remade some changes that i wasn't able to do the right way(the git way)

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,7 @@
 class VideosController < ApplicationController
   before_action :authenticate_user!
   before_action :find_video, only: [:show, :edit, :update, :destroy, :add_view]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @videos = Video.all
@@ -55,5 +56,12 @@ class VideosController < ApplicationController
 
   def video_params
     params.require(:video).permit(:name, :url)
+  end
+
+  def require_same_user
+    if current_user != @video.user
+      flash[:error] = 'Você não tem permissão para executar essa ação'
+      redirect_to root_path
+    end
   end
 end
